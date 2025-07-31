6fab45d947a74e08f97b669b5678dfdd00525ed1
sungq-lee <sungq2@gmail.com>
2025-07-31 11:38:34 +0900

    [refactor] p50 function of generalize step9  완료

--- prime_factors.py
+++ prime_factors.py
@@ -3,11 +3,7 @@ class PrimeFactor:
         factors = []
         if number > 1:
             divisor = 2
-            if number == 4:
-                while number % divisor == 0:
-                    factors.append(divisor)
-                    number //= divisor
-            elif number == 6 or number == 9:
+            if number == 4 or number == 6 or number == 9:
                 while number > 1:
                     while number % divisor == 0:
                         factors.append(divisor)
