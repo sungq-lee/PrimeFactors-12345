9a3343d44af8fc04d7f74fbde2766a25e6b771b0
sungq-lee <sungq2@gmail.com>
2025-07-31 11:27:26 +0900

    [refactor] p40 function of generalize step3  완료

--- prime_factors.py
+++ prime_factors.py
@@ -3,10 +3,7 @@ class PrimeFactor:
         factors = []
         if number > 1:
             if number == 4:
-                if number % 2 == 0:
-                    factors.append(2)
-                    number //= 2
-                if number % 2 == 0:
+                while number % 2 == 0:
                     factors.append(2)
                     number //= 2
             else:
