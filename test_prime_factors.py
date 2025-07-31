65fa336f103115534f96ca043273a8399c2acac3
sungq-lee <sungq2@gmail.com>
2025-07-31 11:28:45 +0900

    [feature] p42 test prime factor of 6 완료

--- test_prime_factors.py
+++ test_prime_factors.py
@@ -19,3 +19,8 @@ def test_prime_factor_of_3():
 def test_prime_factor_of_4():
     prime_factor = PrimeFactor()
     assert prime_factor.of(4) == [2, 2]
+
+
+def test_prime_factor_of_6():
+    prime_factor = PrimeFactor()
+    assert prime_factor.of(6) == [2, 3]
