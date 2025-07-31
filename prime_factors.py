f4589c1f540f02653b4321bedbce84e42db4ad3e
sungq-lee <sungq2@gmail.com>
2025-07-31 11:30:10 +0900

    [refactor] p43 function of generalize step4  완료

--- prime_factors.py
+++ prime_factors.py
@@ -7,8 +7,12 @@ class PrimeFactor:
                     factors.append(2)
                     number //= 2
             elif number == 6:
-                factors.append(2)
-                factors.append(3)
+                while number % 2 == 0:
+                    factors.append(2)
+                    number //= 2
+                while number % 3 == 0:
+                    factors.append(3)
+                    number //= 3
             else:
                 factors.append(number)
         return factors
